22d79184e85c6cc3c7cda8f0f913cdb790acc3eb
Michael Harris <mhguitar1997@gmail.com>
2019-05-08 12:13:51 -0700

    first pass of answering questions for 'this'

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window or global context for "this"
+* 2. implicit is binding in a function
+* 3. explicit is using "call", "apply", or "bind" to get the "this context" from a different object
+* 4. new is using a constructor function that will make a new object and sets "this" to that object's scope
 *
 * write out a code example of each explanation above
 */
