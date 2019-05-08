3af5f016e90c5a6f568e0a90160805254dd0093a
Michael Harris <mhguitar1997@gmail.com>
2019-05-08 13:03:45 -0700

    this assignments MVP

--- assignments/this.js
+++ assignments/this.js
@@ -10,17 +10,72 @@
 */
 
 // Principle 1
-
+this.hey = "Hello";
+console.log(this);
 // code example for Window Binding
 
-// Principle 2
 
+
+// Principle 2
+function intro(){
+    console.log(`Hi, I'm ${this.name} and I'm ${this.age} years old.`);
+}
+const me = {
+    name: "Michael Harris",
+    age : 21,
+    intro: intro
+    // intro: function(){
+    //     return `Hi, I'm ${this.name} and I'm ${this.age} years old.`
+    // }
+}
+const someone = {
+    name: "John Doe",
+    age: 33,
+    intro: intro
+}
+me.intro();
+someone.intro();
+//could also declare funtion outside of object and invoke it using dot notation will work aswell
 // code example for Implicit Binding
 
-// Principle 3
 
+
+// Principle 3
+function Car(attr){
+    this.type = attr.type,
+    this.seats = attr.seats,
+    this.color = attr.color,
+    this.make = attr.make
+    this.carname = attr.carname,
+    this.welcome = function(){
+        return `I drive a ${this.color} ${this.make} ${this.name}, it seats ${this.seats}.`
+    }
+}
+
+const civic = new Car({
+    type: 'sedan',
+    seats: 5,
+    make: 'Honda',
+    carname: 'Civic',
+    color: 'grey',
+})
+const modelX = new Car({
+    seats: 7,
+    make: 'Telsa',
+    color: 'blue',
+    carname: 'Model X'
+})
+console.log(civic.welcome())
+console.log(modelX.welcome())
 // code example for New Binding
 
-// Principle 4
 
+
+// Principle 4
+function whatIDrive(name){
+    return `${name} drives a ${this.seats} seated ${this.make} ${this.carname}`;
+};
+
+console.log(whatIDrive.call(modelX, 'Mark'))
+console.log(whatIDrive.call(civic, 'Sam'))
 // code example for Explicit Binding
